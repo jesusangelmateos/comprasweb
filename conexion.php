2de4724ca113050df2573d36b6bb31e270501072
jesusangelmateos <59920064+jesusangelmateos@users.noreply.github.com>
2020-01-17 13:21:43 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 /* Conexión BD */
-define('DB_SERVER', '10.130.5.120');
+define('DB_SERVER', '10.129.25.230');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
 define('DB_DATABASE', 'comprasweb');
