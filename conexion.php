5fcaa269b2c9dcf2b401830556798d9e43664177
jesusangelmateos <59920064+jesusangelmateos@users.noreply.github.com>
2020-01-17 13:29:36 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', '10.129.25.230');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
-define('DB_DATABASE', 'comprasweb');
+define('DB_DATABASE', 'COMPRASWEB');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
